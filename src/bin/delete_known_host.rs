eb318a5f7f5fb9b8931131d805675ae531205ed3
Max Block <maxblock@pm.me>
2020-09-14 14:45:25 +0300

    update

--- src/bin/delete_known_host.rs
+++ src/bin/delete_known_host.rs
@@ -1,17 +1,21 @@
-use std::env;
-use std::process;
-
+use clap::{App, Arg};
 use dns_lookup::lookup_host;
 
-fn main() {
-    let args: Vec<String> = env::args().collect();
-    if args.len() != 2 {
-        println!("Usage: delete_known_host HOST");
-        process::exit(1);
-    }
+use dev_cli::VERSION;
 
-    let host = args.get(1).expect("");
-    dev_cli::shell_exec(format!("ssh-keygen -R {}", host.as_str()).as_str());
+fn main() {
+    let matches = App::new("Delete hosts from ~/.ssh/known_hosts")
+        .arg(Arg::with_name("hosts").multiple(true).required(true))
+        .version(VERSION)
+        .get_matches();
+
+    for host in matches.values_of("hosts").unwrap().collect::<Vec<_>>() {
+        delete_host(host);
+    }
+}
+
+fn delete_host(host: &str) {
+    dev_cli::shell_exec(format!("ssh-keygen -R {}", host).as_str());
 
     if let Ok(ips) = lookup_host(host) {
         for h in ips.iter() {
--- src/bin/p.rs
+++ src/bin/p.rs
@@ -1,42 +1,57 @@
+use clap::{App, Arg, SubCommand};
+use dev_cli::VERSION;
 use std::env;
 use std::path::Path;
 use std::process;
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    if args.len() < 2 {
-        println!("Usage: p COMMAND [SERVER]");
-        process::exit(1);
-    }
+    let list_sub = SubCommand::with_name("list")
+        .about("List installed packages")
+        .alias("l");
+    let list_outdated_sub = SubCommand::with_name("list-outdated")
+        .about("List outdated packages")
+        .alias("o");
+    let update_sub = SubCommand::with_name("update")
+        .about("Update pip, setuptools, wheel")
+        .alias("u");
+    let venv_sub = SubCommand::with_name("venv")
+        .about("Create venv")
+        .alias("v");
+    let install_sub = SubCommand::with_name("install")
+        .about("Create venv")
+        .alias("i")
+        .arg(
+            Arg::with_name("packages")
+                .required(false)
+                .takes_value(true)
+                .multiple(true),
+        );
 
-    let command = args.get(1).unwrap();
+    let delete_sub = SubCommand::with_name("delete")
+        .about("Delete installed packages for venv")
+        .alias("d");
 
-    match command.as_str() {
-        "o" => {
-            dev_cli::shell_exec("pip list -o");
-        }
-        "l" => {
+    let matches = App::new("python shortcuts")
+        .version(VERSION)
+        .subcommand(list_sub)
+        .subcommand(list_outdated_sub)
+        .subcommand(update_sub)
+        .subcommand(venv_sub)
+        .subcommand(install_sub)
+        .subcommand(delete_sub)
+        .get_matches();
+
+    match matches.subcommand() {
+        ("list", Some(_)) => {
             dev_cli::shell_exec("pip list");
         }
-        "u" => {
+        ("list-outdated", Some(_)) => {
+            dev_cli::shell_exec("pip list -o");
+        }
+        ("update", Some(_)) => {
             dev_cli::shell_exec("pip install -U pip setuptools wheel");
         }
-        "i" => {
-            if env::var_os("VIRTUAL_ENV").is_none() {
-                println!("venv is not activated");
-                process::exit(1);
-            }
-            if args.len() > 2 {
-                for package in &args[2..] {
-                    dev_cli::shell_exec(&format!("pip install {}", package));
-                }
-            } else if Path::new("setup.py").exists() {
-                dev_cli::shell_exec("pip install -Ue .[dev]");
-            } else {
-                dev_cli::shell_exec("pip install -Ur requirements.txt");
-            }
-        }
-        "v" => {
+        ("venv", Some(_)) => {
             if env::var_os("VIRTUAL_ENV").is_some() {
                 println!("venv is activated already");
                 process::exit(1);
@@ -47,7 +62,22 @@ fn main() {
             }
             dev_cli::shell_exec("python -m venv .venv");
         }
-        "d" => {
+        ("install", Some(sub_matches)) => {
+            if env::var_os("VIRTUAL_ENV").is_none() {
+                println!("venv is not activated");
+                process::exit(1);
+            }
+            if sub_matches.values_of("packages").is_some() {
+                for package in sub_matches.values_of("packages").unwrap() {
+                    dev_cli::shell_exec(&format!("pip install {}", package));
+                }
+            } else if Path::new("setup.py").exists() {
+                dev_cli::shell_exec("pip install -Ue .[dev]");
+            } else {
+                dev_cli::shell_exec("pip install -Ur requirements.txt");
+            }
+        }
+        ("delete", Some(_)) => {
             if env::var_os("VIRTUAL_ENV").is_none() {
                 println!("venv is not activated");
                 process::exit(1);
@@ -55,9 +85,6 @@ fn main() {
             dev_cli::shell_exec("pip list --format freeze -e | xargs pip uninstall -y");
             dev_cli::shell_exec("pip freeze | xargs pip uninstall -y");
         }
-        _ => {
-            println!("unknown command: {}", command);
-            process::exit(1);
-        }
+        _ => {}
     }
 }
