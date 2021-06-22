b3e1d0a8012f4367b0b69e491b67acb3516b50d0
Max Block <maxblock@pm.me>
2021-06-22 18:22:16 +0300

    update

---
+++ src/bin/dkh.rs
@@ -0,0 +1,25 @@
+use clap::{App, Arg};
+use dns_lookup::lookup_host;
+use mb_dev::{shell_exec, VERSION};
+
+fn main() {
+    let matches = App::new("dkh")
+        .about("Delete hosts from ~/.ssh/known_hosts")
+        .arg(Arg::new("hosts").multiple(true).required(true))
+        .version(VERSION)
+        .get_matches();
+
+    for host in matches.values_of("hosts").unwrap().collect::<Vec<_>>() {
+        delete_host(host);
+    }
+}
+
+fn delete_host(host: &str) {
+    shell_exec(format!("ssh-keygen -R {}", host).as_str());
+
+    if let Ok(ips) = lookup_host(host) {
+        for h in ips.iter() {
+            shell_exec(format!("ssh-keygen -R {}", h.to_string()).as_str());
+        }
+    }
+}
